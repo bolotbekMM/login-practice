8e882bfd5cb1b8f172f82f2d228cf98756d76ea6
bolotbekMM <bolotbek.muratov@gmail.com>
2021-11-13 10:37:43 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,49 +1,11 @@
-import ExpenseItem from './components/ExpenseItem';
+import React from 'react'
 
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Расходы за обучение',
-      amount: 12000,
-      date: new Date(2021, 9, 10),
-    },
-    {
-      id: 'e2',
-      title: 'Расходы на транспорт',
-      amount: 3000,
-      date: new Date(2021, 9, 2)
-    },
-    {
-      id: 'e3',
-      title: 'Расходы на продукты',
-      amount: 5000,
-      date: new Date(2021, 9, 1),
-    },
-    {
-      id: 'e4',
-      title: 'Расходы на жилье',
-      amount: 20000,
-      date: new Date(2021, 9, 5),
-    },
-  ];
-
-  const addExpenseHandler = (expense) => {
-    console.log(expense);
-  }
-
   return (
     <div>
-      {
-          expenses.map((element, index) => {
-            return <ExpenseItem 
-            title={element.title}
-            amount={element.amount}
-            date={element.date}
-             />
-          })
+      
+    </div>
+  )
 }
-</div>
-  );
-}
-export default App;
+
+export default App
